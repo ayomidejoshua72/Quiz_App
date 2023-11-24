ecead359a73afb1dfc64a82b6b0e02873a48324e
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-11-24 21:45:51 +0100

    split the code into pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizz/welcome_page.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 70, 0, 110),
-                Color.fromARGB(255, 80, 15, 110)
-              ],
-            ),
-          ),
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/quiz-logo.png",
-                  width: 300,
-                  color: const Color.fromARGB(150, 255, 255, 255),
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                const Text("Test yourself with the flutter basic questions!", 
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),),
-                const SizedBox(
-                  height: 40,
-                ),
-                OutlinedButton.icon(
-                  icon: const Icon(Icons.arrow_right_alt),
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.white
-                  ),
-                  onPressed: () {},
-                  label: const Text("Start quiz"),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
+    const WelcomePage()
   );
 }
---
+++ lib/welcome_page.dart
@@ -0,0 +1,60 @@
+import'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 51, 3, 78),
+                Color.fromARGB(255, 128, 7, 184)
+              ],
+            ),
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/quiz-logo.png",
+                  width: 300,
+                  color: const Color.fromARGB(150, 255, 255, 255),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                const Text("Test yourself with the flutter basic questions!", 
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),),
+                const SizedBox(
+                  height: 40,
+                ),
+                OutlinedButton.icon(
+                  icon: const Icon(Icons.arrow_right_alt),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white
+                  ),
+                  onPressed: () {},
+                  label: const Text("Start quiz"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
