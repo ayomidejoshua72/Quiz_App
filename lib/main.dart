6c8b48152eee731f56acf34a2fd462576224b0f2
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-11-24 13:49:04 +0100

    display the background correctly

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,21 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 70, 0, 110), Color.fromARGB(255, 80, 15, 110)],
-          )),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 70, 0, 110),
+                Color.fromARGB(255, 80, 15, 110)
+              ],
+            ),
+          ),
+          child: const SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Text("Quiz App")
+              ],
+            ),
+          ),
         ),
       ),
     ),
