8aec6337fcd15c2081834f7a01e3100d95356938
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-11-24 14:05:54 +0100

    reduce the image opacity

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,29 @@ void main() {
           child: SizedBox(
             width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
                   "assets/images/quiz-logo.png",
                   width: 300,
+                  color: const Color.fromARGB(150, 255, 255, 255),
                 ),
                 const SizedBox(
                   height: 60,
                 ),
-                const Text("Test yourself with the flutter basic questions!"),
+                const Text("Test yourself with the flutter basic questions!", 
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),),
                 const SizedBox(
                   height: 40,
                 ),
                 OutlinedButton.icon(
                   icon: const Icon(Icons.arrow_right_alt),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white
+                  ),
                   onPressed: () {},
                   label: const Text("Start quiz"),
                 )
